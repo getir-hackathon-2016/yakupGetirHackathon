c55a57452754266a99f8daf2c6d8c864429f407c
Yakup Adaklı <yakupa@skyatlas.com>
2016-02-20 01:09:37 +0200

    Map added.

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -17,13 +17,17 @@ angular.module('myApp', ['ngRoute', 'uiGmapgoogle-maps'])
     .config(function(uiGmapGoogleMapApiProvider) {
 
     })
+    .controller('InfoController', function ($scope, $log) {
+      $scope.templateValue = 'hello from the template itself';
+      $scope.clickedButtonInWindow = function () {
+        var msg = 'clicked a window in the template!';
+        $log.info(msg);
+        alert(msg);
+      }
+    })
 
     .controller('MapsController', ['$scope', '$timeout', '$http', 'uiGmapGoogleMapApi', function ($scope, $timeout, $http, uiGmapGoogleMapApi) {
-      var lat = 0;
-      var long = 0;
-
       $scope.onMarkerClicked = function (marker) {
-        console.log('onMarkerClicked');
         marker.showWindow = true;
         $scope.$apply();
         //window.alert("Marker: lat: " + marker.latitude + ", lon: " + marker.longitude + " clicked!!")
@@ -50,18 +54,63 @@ angular.module('myApp', ['ngRoute', 'uiGmapgoogle-maps'])
         $scope.map.markers = [
           {
             id: 1,
-            latitude: 41.0785157,
-            longitude: 29.022107499999994,
+            latitude: $scope.latitude,
+            longitude: $scope.longitude,
             showWindow: false,
             options: {
               animation: 1,
-              labelContent: 'Markers id 1',
+              labelContent: 'Current Location',
               labelAnchor: "22 0",
               labelClass: "marker-labels"
             }
           }
         ];
 
+        $scope.map.clickMarkers = [];
+        $scope.map.clickedMarker = {
+          id: 0,
+          options: {}
+        };
+
+        $scope.map.events = {
+          click: function (mapModel, eventName, originalEventArgs) {
+            console.log('test clcik');
+            var e = originalEventArgs[0];
+            var lat = e.latLng.lat(),
+                lon = e.latLng.lng();
+            $scope.map.clickedMarker = {
+              id: 0,
+              options: {
+                labelContent: 'You clicked here ' + 'lat: ' + lat + ' lon: ' + lon,
+                labelClass: "marker-labels",
+                labelAnchor: "50 0"
+              },
+              latitude: lat,
+              longitude: lon
+            };
+            //scope apply required because this event handler is outside of the angular domain
+            $scope.$apply();
+          },
+          dragend: function () {
+            console.log('test dragend');
+            $timeout(function () {
+              var markers = [];
+
+              var id = 0;
+              if ($scope.map.mexiMarkers !== null && $scope.map.mexiMarkers.length > 0) {
+                var maxMarker = _.max($scope.map.mexiMarkers, function (marker) {
+                  return marker.mid;
+                });
+                id = maxMarker.mid;
+              }
+              for (var i = 0; i < 4; i++) {
+                id++;
+                markers.push(createRandomMarker(id, $scope.map.bounds, "mid"));
+              }
+              $scope.map.mexiMarkers = markers.concat($scope.map.mexiMarkers);
+            });
+          }
+        };
         _.each($scope.map.markers, function (marker) {
           console.log('markers')
           marker.closeClick = function () {
@@ -83,8 +132,8 @@ angular.module('myApp', ['ngRoute', 'uiGmapgoogle-maps'])
           zoom: 8,
           markers: [
             {
-              latitude: 41.0785157,
-              longitude: 29.0221384
+              latitude: 0,
+              longitude: 0
             }
           ]
         };
